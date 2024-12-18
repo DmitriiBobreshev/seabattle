6c0cccd5a8fb954165febd827be2d429cd06a672
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2024-12-18 11:00:45 +0100

    replace static string with consts for localization

--- Localization.cs
+++ Localization.cs
@@ -1,24 +1,24 @@
 static class Localization
 {
-    public static string Missed = "Missed";
-    public static string Hit = "Hit";
-    public static string Dupliacte = "Duplicate";
+    public const string Missed = "Missed";
+    public const string Hit = "Hit";
+    public const string Dupliacte = "Duplicate";
 
-    public static string MainInfo = "To fire type in format ROWCOL. For exit press enter";
-    public static string GameOver = "Game Over. Winner is: ";
-    public static string PressAnyKey = "Press any key to continue";
+    public const string MainInfo = "To fire type in format ROWCOL. For exit press enter";
+    public const string GameOver = "Game Over. Winner is: ";
+    public const string PressAnyKey = "Press any key to continue";
 
-    public static string NotValidCell = "NotValidCell";
-    public static string OneCellShipOutOfRange = "OneCellShip should contain one cell";
-    public static string OneCellShipKilled = "Killed OneCellShip";
-    public static string TwoCellShipOutOfRange = "TwoCellShip should contain tow cell";
-    public static string TwoCellShipKilled = "Killed TwoCellShip";
-    public static string ThreeCellShipOutOfRange = "ThreeCellShip should contain three cell";
-    public static string ThreeCellShipKilled = "Killed ThreeCellShip";
-    public static string FourCellShipOutOfRange = "FourCellShip should contain four cell";
-    public static string FourCellShipKilled = "Killed FourCellShip";
-    public static string ReadLineException = "Expected length 2";
+    public const string NotValidCell = "NotValidCell";
+    public const string OneCellShipOutOfRange = "OneCellShip should contain one cell";
+    public const string OneCellShipKilled = "Killed OneCellShip";
+    public const string TwoCellShipOutOfRange = "TwoCellShip should contain tow cell";
+    public const string TwoCellShipKilled = "Killed TwoCellShip";
+    public const string ThreeCellShipOutOfRange = "ThreeCellShip should contain three cell";
+    public const string ThreeCellShipKilled = "Killed ThreeCellShip";
+    public const string FourCellShipOutOfRange = "FourCellShip should contain four cell";
+    public const string FourCellShipKilled = "Killed FourCellShip";
+    public const string ReadLineException = "Expected length 2";
 
-    public static string ShipNotKilledMessage = "Ship wasn't killed yet";
+    public const string ShipNotKilledMessage = "Ship wasn't killed yet";
 
 }
